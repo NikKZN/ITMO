6735218deb02a08a3cbe072ed4b557e46a479e02
nikolaykozich <nikolay.kozich@adx-team.com>
2022-04-10 12:08:00 +0300

    fixed popup structure in mobile screen, section education with cards and mobile screen structure

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { cardButtons }  from "../scripts/utils/constants.js";
+import { cardButtons, firstButton, secondButton, educationCards, firstCard, secondCard }  from "../scripts/utils/constants.js";
 import { Popup } from "../scripts/components/Popup.js";
 
 const cardPopup = new Popup(".popup");
@@ -9,3 +9,27 @@ cardButtons.forEach(cardButton => {
         cardPopup.setEventListeners();
     })
 });
+
+firstButton.setAttribute("disabled", "disabled"); //disabled button in first rendering
+secondCard.classList.add("education__card_hide"); //hide second card
+
+firstButton.addEventListener('click', () => {
+    firstButton.classList.add("education__button_active");
+    firstButton.setAttribute("disabled", "disabled");
+    secondButton.classList.remove("education__button_active");
+    secondButton.removeAttribute("disabled");
+    educationCards.classList.remove("education__cards_row-reverse");
+    educationCards.classList.add("education__cards_row");
+    secondCard.classList.add("education__card_hide");
+    firstCard.classList.remove("education__card_hide");
+})
+
+secondButton.addEventListener('click', () => {
+    secondButton.classList.add("education__button_active");
+    secondButton.setAttribute("disabled", "disabled");
+    firstButton.classList.remove("education__button_active");
+    firstButton.removeAttribute("disabled");
+    educationCards.classList.add("education__cards_row-reverse");
+    secondCard.classList.remove("education__card_hide");
+    firstCard.classList.add("education__card_hide");
+})
--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -1,1 +1,11 @@
-export const cardButtons = document.querySelectorAll(".card__button");
+export const cardButtons = document.querySelectorAll(".card__button"); // this button opening popup in research-lab section
+
+export const firstButton = document.querySelector(".button1");
+
+export const secondButton = document.querySelector(".button2");
+
+export const educationCards = document.querySelector(".education__cards");
+
+export const firstCard = document.querySelector('#ed-card-1');
+
+export const secondCard = document.querySelector('#ed-card-2');
