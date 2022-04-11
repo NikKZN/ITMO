b1034d2aab7889804133adcfb203a15573998ce5
nikolaykozich <nikolay.kozich@adx-team.com>
2022-04-11 19:06:21 +0300

    edit and modified the code responsible for education section. add partnership section with styles for tablet and mobile

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { cardButtons, firstButton, secondButton, educationCards, firstCard, secondCard }  from "../scripts/utils/constants.js";
+import { cardButtons, firstButton, secondButton, educationCards, firstCard, secondCard}  from "../scripts/utils/constants.js";
 import { Popup } from "../scripts/components/Popup.js";
 
 const cardPopup = new Popup(".popup");
@@ -7,29 +7,25 @@ cardButtons.forEach(cardButton => {
     cardButton.addEventListener('click', () => {
         cardPopup.openPopup();
         cardPopup.setEventListeners();
-    })
+    });
 });
 
-firstButton.setAttribute("disabled", "disabled"); //disabled button in first rendering
-secondCard.classList.add("education__card_hide"); //hide second card
+function toggleButtonAndCardHandler(button1, button2, card1, card2) {
+    button1.classList.add("education__button_active");
+    button1.setAttribute("disabled", "disabled");
 
-firstButton.addEventListener('click', () => {
-    firstButton.classList.add("education__button_active");
-    firstButton.setAttribute("disabled", "disabled");
-    secondButton.classList.remove("education__button_active");
-    secondButton.removeAttribute("disabled");
-    educationCards.classList.remove("education__cards_row-reverse");
+    button2.classList.remove("education__button_active");
+    button2.removeAttribute("disabled");
+
+    card1.classList.toggle("education__card_hide");
+    card2.classList.toggle("education__card_hide");
+
     educationCards.classList.add("education__cards_row");
-    secondCard.classList.add("education__card_hide");
-    firstCard.classList.remove("education__card_hide");
-})
+    educationCards.classList.toggle("education__cards_row-reverse");
+}
 
-secondButton.addEventListener('click', () => {
-    secondButton.classList.add("education__button_active");
-    secondButton.setAttribute("disabled", "disabled");
-    firstButton.classList.remove("education__button_active");
-    firstButton.removeAttribute("disabled");
-    educationCards.classList.add("education__cards_row-reverse");
-    secondCard.classList.remove("education__card_hide");
-    firstCard.classList.add("education__card_hide");
-})
+firstButton.setAttribute("disabled", "disabled"); // disabled button in first rendering
+secondCard.classList.add("education__card_hide"); // hide second card
+
+firstButton.addEventListener('click', () => toggleButtonAndCardHandler(firstButton, secondButton, firstCard, secondCard));
+secondButton.addEventListener('click', () => toggleButtonAndCardHandler(secondButton, firstButton, secondCard, firstCard));
--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -1,11 +1,15 @@
 export const cardButtons = document.querySelectorAll(".card__button"); // this button opening popup in research-lab section
 
-export const firstButton = document.querySelector(".button1");
-
-export const secondButton = document.querySelector(".button2");
-
 export const educationCards = document.querySelector(".education__cards");
 
-export const firstCard = document.querySelector('#ed-card-1');
+export const educationButtons = document.querySelector(".education__buttons")
 
-export const secondCard = document.querySelector('#ed-card-2');
+export const firstButton = educationButtons.firstChild.nextSibling;
+export const secondButton = educationButtons.lastChild.previousSibling;
+
+export const firstCard = educationCards.firstChild.nextSibling;
+export const secondCard = educationCards.lastChild.previousSibling;
+
+
+
+
